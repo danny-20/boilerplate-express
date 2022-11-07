23394f8a1986b3c18ff513295330809146afbc53
danny-20 <57652772+danny-20@users.noreply.github.com>
2022-11-07 15:56:05 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,6 +2,56 @@ let express = require('express');
 let app = express();
 
 
+let response="Hello json";
+ // console.log(mySecret);
+//let absolutePath = __dirname + "/views/index.html"
+//app.get('/', function(req, res){
+//  res.sendFile(__dirname + "/views/index.html")
+  
+//})
+if(process.env.MESSAGE_STYLE === 'uppercase'){
+  response = response.toUpperCase();
+  app.get("/json", function(req, res){
+  res.json({"message":  response})
+
+  });
+  
+}
+else{
+  
+  app.get('/json', function(req, res){
+  res.json({"message": response})
+});
+}
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
